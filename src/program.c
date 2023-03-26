f816c3e22d5f92565de468447a9b5a20af8e5b69
LYHuanh <a7512903@gmail.com>
2023-03-26 13:48:46 +0800

    Auto stash before merge of "develop" and "origin/develop"

---
+++ src/program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>  
+
+int main(){
+        int size=10;
+        int arr[size];
+
+        for(int i=0; i < size; i++){
+            arr[i] = -1;
+            printf("%d", arr[i]);
+        }
+
+        // for (int j = 0; j < size; j++){
+        //     printf("%d", arr[j] );
+        // }
+        
+    
+    return 0;
+}
+
+
+
